0f6d747be85623acdc72f2295dd596454eef0738
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-08-03 05:46:52 -0400

    adicionando jwt

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
-export const nodeEnv: string = process.env.NODE_ENV || 'development';
-export const port: string = process.env.PORT || '8080';
-export const databaseUrl: string = (nodeEnv === 'test')
+export const nodeEnv = process.env.NODE_ENV || 'development';
+export const port = process.env.PORT || '8080';
+export const databaseUrl = (nodeEnv === 'test')
   ? 'postgres://postgres:postgres@localhost:5432/smart-plug-test'
   : process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/smart-plug';
+export const secret = process.env.SECRET || 's3cr3t';
+export const expiresIn = process.env.EXPIRES_IN || '604800';
