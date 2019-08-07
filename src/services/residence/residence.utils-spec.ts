2cef6247a9af1b8e99212e6d3f838fe9260ae964
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-08-07 18:28:50 -0400

    colocando residence name com mais de uma palavra

--- src/services/residence/residence.utils-spec.ts
+++ src/services/residence/residence.utils-spec.ts
@@ -1,4 +1,5 @@
 import faker from 'faker';
+import { Validator } from 'class-validator';
 import { UserEntity } from '../user';
 
 
@@ -12,8 +13,15 @@ interface ResidenceOptions {
   admin: UserEntity;
 }
 
+const validator = new Validator();
+
+const nameValid = (): string => {
+  const name = faker.lorem.words(10).substr(0, 36).toLowerCase();
+  if (validator.maxLength(name, 50)) return name;
+  return nameValid();
+};
 
 export const ResidenceFactory = (options: ResidenceOptions): ResidenceFactory => ({
-  name: options.name || faker.lorem.word().toLowerCase(),
+  name: options.name || nameValid(),
   admin: options.admin,
 });
