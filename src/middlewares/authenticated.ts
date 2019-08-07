d30238c2f5a896469f337afe699871c416fbf9bd
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-08-07 19:10:44 -0400

    passando o userId após autenticação

--- src/middlewares/authenticated.ts
+++ src/middlewares/authenticated.ts
@@ -5,14 +5,22 @@ import { UnauthorizedError } from 'restify-errors';
 import { verify } from 'jsonwebtoken';
 import { secret } from '../config';
 
+export interface RequestAuthenticated extends Request {
+  userId?: number;
+}
 
-export = (req: Request, res: Response, next: Next): void => {
+interface Decoded { id: number }
+
+const authenticated = (req: RequestAuthenticated, res: Response, next: Next): void => {
   try {
     const token = req.header('cookie').split('=')[1].split(';')[0];
-    verify(token, secret);
+    const { id } = verify(token, secret) as Decoded;
+    req.userId = id;
     next();
   } catch (error) {
     res.setHeader('Set-Cookie', 'token=; HttpOnly; Path=/');
     next(new UnauthorizedError('Acesso negado'));
   }
 };
+
+export default authenticated;
